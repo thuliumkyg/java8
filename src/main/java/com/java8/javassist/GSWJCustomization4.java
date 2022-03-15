649d89279ba1f44be49200ef2cdd3d354f9caf61
geky <geky@esupplychain.com>
2022-03-15 17:56:11 +0800

    javassist 文档学习 4

---
+++ src/main/java/com/java8/javassist/GSWJCustomization4.java
@@ -0,0 +1,23 @@
+package com.java8.javassist;
+
+import javassist.ClassPool;
+import javassist.Loader;
+import javassist.Translator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ *
+ * @author  bingshan
+ * @date 2022/3/13 15:57
+ */
+public class GSWJCustomization4 {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GSWJCustomization4.class);
+
+
+    public static void main(String[] args) throws Throwable {
+
+    }
+}
