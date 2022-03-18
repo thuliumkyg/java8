8ca646536d512be898b8616eea860fe54168986f
geky <geky@esupplychain.com>
2022-03-18 15:09:08 +0800

    javassist 文档学习 4

---
+++ src/main/java/com/java8/javassist/clazz/Point.java
@@ -0,0 +1,11 @@
+package com.java8.javassist.clazz;
+/**
+ *
+ *
+ * @author  bingshan
+ * @date 2022/3/16 20:45
+ */
+public class Point {
+    int x, y;
+    void move(int dx, int dy) { x += dx; y += dy; }
+}
