6a26a445de113545cd8d2f9aef604063cddfd9b4
geky <geky@esupplychain.com>
2022-05-27 13:37:45 +0800

    java11 新特性

---
+++ src/com/java11/Java11.java
@@ -0,0 +1,50 @@
+package src.com.java11;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * @author bingshan
+ * @date 2022/5/6 13:44
+ */
+public class Java11 {
+
+    public static void main(String[] args) {
+
+        /** 1、JShell */
+
+
+        /** 2、Dynamic Class-File Constants 类文件新添的一种结构 */
+
+
+        /** 3、增强局部变量类型推断var */
+        Arrays.asList("Java", "Python", "Ruby")
+                .forEach((var s) -> System.out.println("Hello, " + s));
+
+        /** 4、新加一些使用的API
+         *      1) 新的本机不可修改集合 APi
+         *      2) Stream 加强
+         *      3） 增加了一系列的字符串处理方法
+         *      4） Optional 加强
+         *      5） 改进的文件 API
+         */
+
+
+        /** 5、移除的一些其他内容 */
+
+        /** 6、标准Java异步HTTP客户端 */
+
+        /** 7、更简化的编译运行程序 */
+
+        /** 8、Unicode 10 */
+
+        /** 6、标准Java异步HTTP客户端 */
+
+
+
+    }
+
+}
